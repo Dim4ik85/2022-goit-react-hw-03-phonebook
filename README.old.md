ba14abc40a75334f80644dc99c3b9003e2f40b64
Dim4ik85 <lafuma4me@gmail.com>
2022-04-21 23:58:11 +0300

    update

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# 2022-goit-react-hw-03-phonebook
---
+++ src/App.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import shortid from 'shortid';
+import ContactForm from './components/ContactForm';
+import Filter from './components/Filter';
+import ContactList from './components/ContactList';
+import styles from './App.module.css';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  handleNameChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  repeatName = newName => {
+    return this.state.contacts.find(({ name }) => name === newName);
+  };
+
+  //через name
+  // deleteContact = oldName => {
+  //   this.setState({
+  //     contacts: this.state.contacts.filter(({ name }) => name !== oldName),
+  //   });
+  // };
+
+  deleteContact = contactId => {
+    this.setState({
+      contacts: this.state.contacts.filter(({ id }) => id !== contactId),
+    });
+  };
+
+  formSubmitHandler = (name, number) => {
+    if (!this.repeatName(name)) {
+      const contact = {
+        id: shortid.generate(),
+        name,
+        number,
+      };
+      this.setState(prevState => ({
+        contacts: [contact, ...prevState.contacts],
+      }));
+    } else {
+      alert(`${name} is already in contacts`);
+    }
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) this.setState({ contacts: parsedContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  render() {
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+    return (
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitHandler} />
+        <h2 className={styles.titleContacts}>Contacts</h2>
+        <Filter filter={filter} onInputChange={this.handleNameChange} />
+        {visibleContacts.length > 0 && (
+          <ContactList
+            contacts={visibleContacts}
+            onDeleteContact={this.deleteContact}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
